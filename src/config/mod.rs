69df42200393ae6c46757a2c48cdc4b9268f089d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-04-09 11:38:24 +0200

    Update `config.toml` path

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,6 +4,7 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::fs;
 
 use clap::Parser;
 use nostr_sdk::Url;
@@ -14,17 +15,30 @@ pub mod model;
 pub use self::model::Config;
 use self::model::{ConfigFile, Limit, Network, Nostr, Redis};
 
+
 fn default_dir() -> PathBuf {
     let home: PathBuf = dirs::home_dir().unwrap_or_else(|| {
         panic!("Unknown home directory");
     });
-    home.join(".ndk-rest")
+    let path = home.join(".nostr-rest");
+    fs::create_dir_all(&path).expect("Impossible to create default dir");
+    path
 }
 
 fn default_config_file() -> PathBuf {
     let mut default = default_dir().join("config");
     default.set_extension("toml");
-    default
+
+    if default.exists() {
+        default
+    } else {
+        let path = PathBuf::from("config.toml");
+        if path.exists() {
+            path
+        } else {
+            panic!("Config file not found.");
+        }
+    }
 }
 
 #[derive(Parser)]
@@ -38,13 +52,8 @@ impl Config {
     pub fn get() -> Self {
         let args: Args = Args::parse();
 
-        let config_file_path: PathBuf = match args.config {
-            Some(path) => path,
-            None => default_config_file(),
-        };
-
-        let content =
-            std::fs::read_to_string(config_file_path).expect("Impossible to read config file");
+        let config_file_path: PathBuf = args.config.unwrap_or_else(|| default_config_file());
+        let content = fs::read_to_string(config_file_path).expect("Impossible to read config file");
         let config_file: ConfigFile =
             toml::from_str(&content).expect("Impossible to parse config file");
 
